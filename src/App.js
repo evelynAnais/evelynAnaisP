d3aec1001164e71f7492180cf43ec5e35c06ef13
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-01 18:00:35 -0600

    trianagle animation with css

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ export default function App() {
       <Contact />
       <Footer />
     </main>
-
   );
 }
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,29 @@ import './Home.css';
 export default function Home() {
   return(
     <section>
-      <p>Home</p>
+      <div className="arrowSliding">
+        <div className='tri-one'></div>
+      </div>
+      
+      <div class="arrow-down"></div>
+      <div class="arrow-left"></div>
+      <div class="arrow-right"></div>
+
+      <div id="arrowAnim">
+        <div className="arrowSliding">
+          <div className="arrow"></div>
+        </div>
+        <div className="arrowSliding delay1">
+          <div className="arrow"></div>
+        </div>
+        <div className="arrowSliding delay2">
+          <div className="arrow"></div>
+        </div>
+        <div className="arrowSliding delay3">
+          <div className="arrow"></div>
+        </div>
+      </div>
+
     </section>
   );
 }
