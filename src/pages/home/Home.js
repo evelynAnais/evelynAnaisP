db56f80dbefc888d5fc59697e4cb46cbecc6d078
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-01 20:47:12 -0600

    added media query

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,19 +3,19 @@ import './Home.css';
 export default function Home() {
   return(
     <section>
-      <div className="arrowSliding">
+      <div className='arrowSliding'>
         <div className='tri-one'></div>
       </div>
-      <div className="arrowSliding2">
-        <div className="tri-two"></div>
+      <div className='arrowSliding2'>
+        <div className='tri-two'></div>
       </div>
-      <div className="tri-three"></div>
-      <div className="arrowSliding2 absolute">
-        <div className="tri-four"></div>
+      <div className='tri-three'></div>
+      <div className='arrowSliding2 absolute'>
+        <div className='tri-four'></div>
       </div>
       <div className='absolute-intro'>
-        <h1>Hello! I am Evelyn</h1>
-        <h4>I am a full stack Software Engineer</h4>
+        <h1>Hello, I'm Evelyn!</h1>
+        <h4>I'm a full stack Software Engineer</h4>
       </div>
     </section>
   );
