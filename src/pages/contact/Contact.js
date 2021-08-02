900164039d66a9317dc1faa4b4005c2197640cc7
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-01 21:32:05 -0600

    added animation

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -3,8 +3,12 @@ import './Contact.css';
 export default function Contact() {
   return(
     <section>
+      <div className='arrowSliding3'>
+          <div className='tri-one2'></div>
+      </div>
       <p>
         Contact
+        
       </p>
     </section>
   );
