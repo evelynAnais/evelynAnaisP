dbf0958ef2c662ff0928e63ec1612d8565c6acd8
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-03 22:13:02 -0600

    fremoved placeholder in project card

--- src/pages/projects/ProjectCard.js
+++ src/pages/projects/ProjectCard.js
@@ -6,7 +6,7 @@ export default function ProjectCard({ item }) {
           <img src="..." className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
-            <p className="card-text">{item?.comment}Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className="card-text">{item?.comment}</p>
           </div>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">An item</li>
