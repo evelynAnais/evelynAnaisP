9ee42410f8221cc2ee2275f3c3efeafff9cab6c8
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-08-12 22:40:02 -0600

    added card for portfolio tech stack

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react';
 import { getProject } from '../../utils/api';
+import Loading from '../../components/Loading';
 import ProjectList from './ProjectList';
 import './Projects.css';
 
 export default function Projects() {
   const [project, setProject] = useState([]);
-  
+  const [loading, setLoading] = useState(false);
+
   function projects() {
-    getProject().then((res) => setProject(res.data));
+    getProject().then((res) => {
+      setProject(res.data)
+      setLoading(false);
+    });
   }
   useEffect(projects, []);
 
@@ -21,7 +26,21 @@ export default function Projects() {
         </div>
       </div>
       <ProjectList project={project} />
+      {loading && <Loading />}
+      <div className='d-flex flex-column justify-content-center align-items-center py-4'>
+        <div className='card border-0 bg-transparent projectSize'>
+          <div className='card-body px-0 py-0'>
+            <blockquote className='blockquote'>
+              <hr/>
+              <footer className='blockquote-footer'>Portfolio Tech Stack 
+                <cite title='Source Title'> <br/>Javascript, Express, PostgreSQL, Node.js, Knex, React, HTML5, CSS3, Bootstrap
+                </cite>
+                <hr/>
+              </footer>
+            </blockquote>
+          </div>
+        </div>  
+      </div>
     </section>
-    
   );
 }
